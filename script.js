1d386691d9b065f2200be797979e61f6e59c2fcb
gaglaniyash <gaglaniyash755@gmail.com>
2024-12-06 12:56:19 +0530

    file loaded

---
+++ script.js
@@ -0,0 +1,92 @@
+// let arr = [3, 5, 20, 40, 8];
+// let b = arr.map((element, index) => {
+//     return element * 10;
+// })
+// console.log(b);
+
+// let arr1 = [23, 45, 20, 40, 78];
+// let x = arr.map((element, index) => {
+//     return element % 10;
+// })
+// console.log(x);
+
+// console.log(-------------------------ans-1-------------------------)
+// let arr = [3, 6, 9, 15, 32];
+// let newarr = arr.map((el, i) => {
+//     return el * 2;
+// })
+// console.log(newarr);
+
+// console.log(-------------------------ans-2-------------------------)
+// let array = [10, 20, 30, 40, 50];
+// array.splice(2, 1);
+// console.log(array);
+
+// console.log(-------------------------ans-3-------------------------)
+// let city = ["Amreli", "Rajkot", "Surat", "Diu", "Mumbai", "Pune"];
+// let newCity = city.filter((city) => city.length > 5);
+// console.log(newCity);
+
+// console.log(-------------------------ans-4-------------------------)
+// let checkNumber = [1, 5, 10, 15];
+// console.log(checkNumber.includes(10));
+
+// console.log(-------------------------ans-5-------------------------)
+// let nums = [5, 10, 15, 20, 25];
+// const number = nums.findIndex((el) => el > 18);
+// console.log(number);
+
+// console.lg(-------------------------ans-6-------------------------)
+// let fruits = ["apple", "banana"];
+// fruits.unshift("mango", "grapes", "oranges");
+// console.log(fruits);
+
+// console.log(-------------------------ans-7-------------------------)
+// function areAllEven(arr) {
+//     return arr.every(num => num % 2 === 0);
+// }
+// const numbers = [2, 4, 6, 8];
+// console.log(areAllEven(numbers));
+
+// console.log(-------------------------ans-8-------------------------)
+// let checkArr = [3, 5, 7, 8];
+// let result = ((element) => element % 2 == 0);
+// console.log(checkArr.some(result));
+
+// console.log(-------------------------ans-9-------------------------)
+// let colors = ["red", "blue", "green", "yellow"];
+// colors.splice(1, 2, "purple", "orange");
+// console.log(colors);
+
+// console.log(-------------------------ans-10-------------------------)
+// let countArr = [7, 1, 7, 2, 7, 3];
+// let count = 0;
+// let result = countArr.forEach((el, i) => {
+//     if(el == 7){
+//         count++;
+//     }
+// });
+// console.log(count);
+
+// console.log(-------------------------ans-11-------------------------)
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+// }
+// console.log(reverseString("hello"));
+
+// console.log(-------------------------ans-12-------------------------)
+// let str = "           Hello World!            ";
+// console.log(str.trim(str));
+
+// console.log(-------------------------ans-13-------------------------)
+// let words = ["Javascript", "is", "awesome"];
+// console.log(words.join("-"));
+
+// console.log(-------------------------ans-14-------------------------)
+// let string = "apple,banana,grapes";
+// const result = string.split(',')
+// console.log(string);
+
+// console.log(-------------------------ans-15-------------------------)
+// let str1 = "Javascript";
+// console.log(str1.indexOf('a'));
